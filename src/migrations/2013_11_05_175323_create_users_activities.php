3ec3b8b95f687466f8016ffd4fdcb194cfdb8751
Jan Kivi <jankivi@kapsi.fi>
2013-11-05 19:58:32 +0200

    ...

--- src/migrations/2013_11_05_175323_create_users_activities.php
+++ src/migrations/2013_11_05_175323_create_users_activities.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersActivitiesTable extends Migration {
+class CreateUsersActivities extends Migration {
 
 	public function up() {
 		Schema::create('users_activities', function(Blueprint $table) {
